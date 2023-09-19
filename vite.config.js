9521e7617a9eb724fbec02433134d300a3f31231
Jimmy Zhang <jimmy.zhang@perficient.com>
2023-09-19 14:30:28 -0400

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+    coverage: {
+      reporter: ['text', 'html'],
+    },
+  },
+});
